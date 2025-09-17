47ecdce6dc96d3c7e79727d070ed2f08ee0662c8
Alvyn <alvynh7@gmail.com>
2025-09-17 12:11:27 +0200

    Ajout d'image ascii

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6178e3a66f2cf82cc55289569cd8259e303e50a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b8396caaab9b16e50834673192dd8f027af10f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
