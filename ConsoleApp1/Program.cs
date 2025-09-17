2b8396caaab9b16e50834673192dd8f027af10f1
sabri <younes.sabri670@gmail.com>
2025-09-17 12:05:30 +0200

    Ajout du titre en ASCII

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
         String choix;
         String texte;
         texte = File.ReadAllText(@"C:\Users\Elève\Documents\INFETProjet\JAT\Ressources\intro.txt");
@@ -19,8 +20,11 @@ class Program
         string pseudo = Console.ReadLine();
         Console.WriteLine("OK " + pseudo + ", quel est ton style de combat ?");
         Console.WriteLine("1. Archer (Mauvais en combat rapproché mais bon à distance)");
+
         Console.WriteLine("2. Guerrier (Bon en combat rapproché mais mauvais à distance)");
+
         Console.WriteLine("3. Mage (Moyen en combat rapproché et à distance mais avec des sorts)");
+        
         choix = Console.ReadLine();
         if (choix == "1")
         {
@@ -36,10 +40,10 @@ class Program
         }
         else
         {
-            Console.WriteLine("Choix invalide, recommence.");
-            return;
+            Console.WriteLine("Choix invalide, choisis entre 1, 2 et 3.");
+        
 
         
-        
+        }
     }
 }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872dd9dc5e4df34a043a4293b24a1a393f4453c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6178e3a66f2cf82cc55289569cd8259e303e50a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
